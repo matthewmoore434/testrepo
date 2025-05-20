d55cc3d6fb42be1b774e097aba4e93deeec3a9c7
S277144 <s277144@uos.ac.uk>
2025-05-20 14:41:28 +0100

    my second committ

---
+++ gittutiorals.js
@@ -0,0 +1,38 @@
+
+
+/*  
+
+1- Install a git on your local machine 
+
+2 - created a repo on the remote address
+
+3 - Clone the repo on your local machine  git clone  repo-url
+
+
+4 - did a some changes in our empty repo
+
+5 - now we are checking the git status and git commit
+
+> git status  ---> for the check which files have been changed( modified, added, deleted)
+
+--> move the git changes into the staged changes for the commit
+
+git add -A
+
+--> now we are committing our first commit 
+
+
+git commit -m ~my first committ~
+
+
+For branch check in which currently we are in the branch 
+
+git branch
+
+
+now we need to push changes on the git 
+
+git push origin main 
+
+
+*/
